2d2471bdd7702d15f55716addd9cdfb4e5b90720
BSsAmal <bsassiamal@gmail.com>
2021-05-11 12:10:40 +0100

    v1

---
+++ client/src/Index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+const element = <App />;
+const container = document.getElementById("app");
+ReactDOM.render(element, container);
